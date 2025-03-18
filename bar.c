a9b3d01b08916ee3ad8ba59ea0e0704d76fd4146
raul <raul.araujo.101@ufrn.edu.br>
2025-03-18 17:15:01 -0300

    cria tela de cadastro

--- bar.c
+++ bar.c
@@ -3,13 +3,27 @@
 int main (void) {
 
     //TELA PRINCIPAL
-    printf("#===========================# \n");
-    printf("|                           | \n");
-    printf("|       1 - CADASTRO        | \n");
-    printf("|       2 - LOGIN           | \n");
-    printf("|       3 - SAIR            | \n");
-    printf("|                           | \n");
-    printf("#===========================# \n");
+    printf("#============================# \n");
+    printf("|                            | \n");
+    printf("|        1 - CADASTRO        | \n");
+    printf("|        2 - LOGIN           | \n");
+    printf("|        3 - SAIR            | \n");
+    printf("|                            | \n");
+    printf("#============================# \n");
+
+    //TELA DE CADASTRO
+    printf("#============================# \n");
+    printf("|           CADASTRO         | \n");
+    printf("|                            | \n");
+    printf("|  Nome:                     | \n");
+    printf("|  Sobrenome:                | \n");
+    printf("|  Data de nascimento:       | \n");    
+    printf("|  CPF:                      | \n");
+    printf("|  E-mail:                   | \n");
+    printf("|  Login desejado:           | \n");
+    printf("|  Senha desejada:           | \n");
+    printf("|                            | \n");
+    printf("#============================# \n");
 
     return 0;
 }
