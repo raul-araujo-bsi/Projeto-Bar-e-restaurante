b37c255febe13ae14f0834de85a17b95f9029313
Raul Araújo <raul.araujo.101@ufrn.edu.br>
2025-04-05 14:29:03 -0300

    ajusta tela sobre

--- bar.c
+++ bar.c
@@ -5,17 +5,11 @@
 
 //ASSINATURA DAS FUNÇÕES
 char tela_principal(void);
-// void tela_sobre(void);
-// void gestao(void);
-// void cadastro_produto(void);
-// void produtos(void);
+void sobre(void);
 void saida(void);
 
 //PROGRAMA PRINCIPAL
 int main (void) {
-//     tela_informacoes();
-//     return 0;
-// }
     char opcao;
     
     do {
@@ -29,7 +23,7 @@ int main (void) {
                       break;
             case '4': //modulo_relatorios();
                       break;
-            case '5': //sobre();
+            case '5': sobre();
         }
     } while (opcao != '0');
     saida();
@@ -50,24 +44,26 @@ char tela_principal(void) {
     getchar();
     return op;
 }
-// //MÓDULO DE INFORMAÇÕES DO PROGRAMA
-// void tela_sobre(void) {
-//     printf("#===========================================================# \n");
-//     printf("|        SISTEMA DE GESTÃO PARA BARES E RESTAURANTES        | \n");
-//     printf("|                                                           | \n");
-//     printf("|           Desenvolvido por Raul Breno de Araújo           | \n");
-//     printf("| Este programa utiliza funcionalidades necessárias para a  | \n");
-//     printf("|gestão de bares e restaurantes, conectando as necessidades | \n");
-//     printf("|do cliente aos serviços oferecidos pelo estabelecimento.   | \n");
-//     printf("|                                                           | \n");
-//     printf("|               Informações do desenvolvedor                | \n");
-//     printf("|                                                           | \n");
-//     printf("| Nome: Raul Breno de Araújo                                | \n");
-//     printf("| E-mail: raul.araujo.101@ufrn.edu.br                       | \n");
-//     printf("|                                                           | \n");
-//     printf("#===========================================================# \n");
-// }
-// //MÓDULO DE SAÍDA DO PROGRAMA
+
+
+void sobre(void) {
+    printf("#===========================================================# \n");
+    printf("|        SISTEMA DE GESTÃO PARA BARES E RESTAURANTES        | \n");
+    printf("|                                                           | \n");
+    printf("|           Desenvolvido por Raul Breno de Araújo           | \n");
+    printf("| Este programa utiliza funcionalidades necessárias para a  | \n");
+    printf("|gestão de bares e restaurantes, conectando as necessidades | \n");
+    printf("|do cliente aos serviços oferecidos pelo estabelecimento.   | \n");
+    printf("|                                                           | \n");
+    printf("|               Informações do desenvolvedor                | \n");
+    printf("|                                                           | \n");
+    printf("| Nome: Raul Breno de Araújo                                | \n");
+    printf("| E-mail: raul.araujo.101@ufrn.edu.br                       | \n");
+    printf("|                                                           | \n");
+    printf("#===========================================================# \n");
+}
+
+
 void saida(void) {
     printf("#============================# \n");
     printf("|                            | \n");
@@ -76,35 +72,3 @@ void saida(void) {
     printf("|                            | \n");
     printf("#============================# \n");
 }
-// //MÓDULO PARA GESTORES
-// void modulo_gestao(void) {
-//     printf("#============================# \n");
-//     printf("|                            | \n");
-//     printf("|  1 - Cadastrar produtos    | \n");
-//     printf("|  2 - Ver produtos          | \n");
-//     printf("|  3 - Editar produtos       | \n");
-//     printf("|  4 - Excluir produtos      | \n");
-//     printf("|  5 - Relatórios            | \n");
-//     printf("|  0 - Sair                  | \n");
-//     printf("|                            | \n");
-//     printf("#============================# \n");
-// }
-// void cadastro_produto(void) {
-//     printf("#============================# \n");
-//     printf("|    CADASTRO DE PRODUTOS    | \n");
-//     printf("|                            | \n");
-//     printf("|  Id:                       | \n");
-//     printf("|  Fornecedor:               | \n");
-//     printf("|  Produto:                  | \n");
-//     printf("|  Quantidade:               | \n");
-//     printf("|  Valor:                    | \n");
-//     printf("|                            | \n");
-//     printf("#============================# \n");
-// }
-// void produtos(void) {
-//     printf("#=================================# \n");
-//     printf("|              PRODUTOS           | \n");
-//     printf("#=================================# \n");
-//     printf("   Id   Produto            Valor    \n");
-//     printf("#=================================# \n");
-// }
