4f8843402b17f2237cefa8df6a66cd473ac48d07
Raul Araújo <raul.araujo.101@ufrn.edu.br>
2025-04-17 15:49:23 -0300

    limpa valida nome

--- valida.c
+++ valida.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
-#include "valida.h"
 
 int valida_nome (char *nome){
-    int i = 0;
-    do {
-        if (!isalpha(nome[i]) && nome[i] != ' ') {
-            return 0;
-        }
-        i++;
-    } while (nome[i] != '\0');
-    return 1;
+
 }
 
 
-// void valida_cpf (void){
+int valida_cpf (void){
 
-// }
-
-
-// void valida_fone (void) {
-
-// }
+}
 
 
-// void valida_email (void){
+int valida_fone (void) {
 
-// }
+}
+
+
+int valida_email (void){
+
+}
