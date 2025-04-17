1298c333bcc3d4f2707a1ee5bc80afc43e5f8683
Raul Araújo <raul.araujo.101@ufrn.edu.br>
2025-04-17 14:31:22 -0300

    desativa funções

--- valida.h
+++ valida.h
@@ -1,4 +1,4 @@
 int valida_nome (char *nome);
-void valida_cpf (void);
-void valida_fone (void);
-void valida_email (void);
+// void valida_cpf (void);
+// void valida_fone (void);
+// void valida_email (void);
