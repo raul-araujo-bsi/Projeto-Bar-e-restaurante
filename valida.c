b539ca98aeaf7bb9356ac7456a4efd39e76a2952
Raul Araújo <raul.araujo.101@ufrn.edu.br>
2025-04-21 20:28:20 -0300

    atualiza valida nome

--- valida.c
+++ valida.c
@@ -2,7 +2,17 @@
 #include <ctype.h>
 
 int valida_nome (char *nome){
-
+    int i;
+    i = 0;
+    do {
+        if (nome[0] == '\0') {
+        return 0;
+        } else if (!isalpha(nome[i]) && nome[i] != ' ') {
+            return 0;
+        }
+        i++;
+    } while (nome[i] != '\0');
+    return 1;
 }
 
 
