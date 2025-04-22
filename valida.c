667b3311bcbc6af98fb38dc35a0956adab71da17
Raul Araújo <raul.araujo.101@ufrn.edu.br>
2025-04-21 21:34:58 -0300

    atualiza valida email

--- valida.c
+++ valida.c
@@ -39,7 +39,6 @@ int valida_fone (char *fone) {
     if (strlen(fone) != 11) {
         return 0;
     }
-
     i = 0;
     do {
         if (!isdigit(fone[i])) {
@@ -47,11 +46,31 @@ int valida_fone (char *fone) {
         }
         i++;
     } while (fone[i] != '\0');
-
     return 1;
 }
 
 
-// int valida_email (void){
-
-// }
+int valida_email (char *email){
+    int i = 0;
+    int arroba = 0;
+    int ponto = 0;
+    if (email[0] == '@' || email[0] == '.') {
+        return 0;
+    }
+    do {
+        if (email[i] == " ") {
+            return 0;
+        } else if (email[i] == "." && arroba < 0) {
+            return 0;
+        }
+        else if (email[i] == "@") {
+            arroba++;
+        } else if (email[i] == "." && arroba > 0) {
+            ponto++;
+        }
+        i++;
+    } while (email[i] != '\0');
+    if (arroba == 1 && ponto >= 1) {
+        return 1;
+    }
+    return 0;
