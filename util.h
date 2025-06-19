f656e601e8817b648fe34b8b2709a8319c83ef52
Raul <raul.araujo.101@ufrn.edu.br>
2025-06-19 14:55:22 -0300

    adiciona gravação de cliente e produto

--- util.h
+++ util.h
@@ -1,3 +1,7 @@
+#include "cliente.h"
+#include "produto.h"
+
 void limpaTela(void);
-void grava_cliente(Cliente*cli);
+void grava_cliente(Cliente* cli);
+void grava_produto(Produto* prod);
 int gera_id(void);
